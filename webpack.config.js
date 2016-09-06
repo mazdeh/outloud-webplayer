c5253f9d1b764a9e6b45ba3d2cbde27c134436f7
mazdeh <vahid.mazdeh@gmail.com>
2016-09-06 13:19:17 -0600

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    node: {
+      fs: "empty"
+    }
+};
